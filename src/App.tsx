22f77484ba4a334409bfc2857bc09e309572ef48
kikrolik <jakub.m.slowik@gmail.com>
2025-10-23 13:17:26 +0200

    feat: initial Vite React TypeScript setup
    - Add package.json with React and TypeScript dependencies
    - Configure Vite build system
    - Add TypeScript configuration
    - Create basic React app structure
    - Add basic styling

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Mapa Polski</h1>
+      <p>Aplikacja mapowa z OpenLayers - wkrótce będzie dostępna!</p>
+    </div>
+  )
+}
+
+export default App
